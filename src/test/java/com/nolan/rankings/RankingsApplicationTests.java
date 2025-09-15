7490106fec5b0cca88bfde0d33ec7a13e851432e
Nolan O'Shea <nolano14@gmail.com>
2025-09-15 17:36:47 -0500

    initial commit

---
+++ src/test/java/com/nolan/rankings/RankingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolan.rankings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
